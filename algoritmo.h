12b58d282302adc2c5b198367b8efc91a8699ba5
Milena0899 <milenamantovanelli7@gmail.com>
2022-02-12 00:54:57 -0300

    quicksort decrescendo

--- algoritmo.h
+++ algoritmo.h
@@ -52,7 +52,7 @@ ordenacao*          shellSort(int qtd, long int *dados);
 * Pre-Condicao: Nenhuma.
 * Pos-Condicao: Retornar um ponteiro de ordenação alocado dinamicamente.
 */
-ordenacao*          quickSort(long int *dados, long int esq, long int dir, ordenacao* quick);
+ordenacao*          quickSort(long int *dados, int esq, int dir, ordenacao* quick);
 /*
 * Função heapSort:
 * Inputs: Quantidade de dados e base de dados.
